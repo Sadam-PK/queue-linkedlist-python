1a90a4c68b494ec6c149ca49d089c004ba4061ad
sadamccr <sadamccr@gmail.com>
2023-02-08 01:47:18 +0500

    reformated and done

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ class Node:
         self.data = data
         self.next = None
 
+
 class Queue:
     def __init__(self):
         self.head = None
@@ -36,19 +37,20 @@ class Queue:
             return None
         current = self.head
         while current:
-            print(current.data, end = " ")
+            print(current.data, end=" ")
             current = current.next
         print()
+
 
 # example usage
 queue = Queue()
 queue.enqueue(1)
 queue.enqueue(2)
 queue.enqueue(3)
-queue.display() # outputs 1 2 3
-print(queue.dequeue()) # outputs 1
-print(queue.dequeue()) # outputs 2
-queue.display() # outputs 3
-print(queue.dequeue()) # outputs 3
-print(queue.dequeue()) # outputs Error: Queue is empty.
-queue.display() # outputs Error: Queue is empty.
+queue.display()  # outputs 1 2 3
+print(queue.dequeue())  # outputs 1
+print(queue.dequeue())  # outputs 2
+queue.display()  # outputs 3
+print(queue.dequeue())  # outputs 3
+print(queue.dequeue())  # outputs Error: Queue is empty.
+queue.display()  # outputs Error: Queue is empty.
